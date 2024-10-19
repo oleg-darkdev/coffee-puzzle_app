6a7b34af894af07f808c152842834257bd5db3a9
DarkDev <darkdev_brand@riseup.net>
2024-10-19 08:19:45 +0300

    fix bug

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,17 +2,17 @@
 
 <script>
 	import { dev } from '$app/environment';
-	export let error, status;
+	// export const status;
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	<title>{status}</title>
 </svelte:head>
 
 <div class="col-md-9">
 	<h1>{status}</h1>
 
-	<!-- <p>{error.message}</p> -->
+	<p>{error.message}</p>
 
 
-</div>
+</div> -->
