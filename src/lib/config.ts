0fe2c19284784f132d0b3ace9c5cc5ffa1f9e1be
DarkDev <darkdev_brand@riseup.net>
2024-10-19 08:23:22 +0300

    clear legacy

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
 export const config = {
-	baseUrl: import.meta.env.VITE_BASE_URL
+	baseUrl: ''
 };
 
 export const siteSeoBase = {
 	title: 'darkDev starter kit',
 	description: '',
 	keywords: '44games, ',
-	canonical: import.meta.env ? import.meta.env.VITE_SITE_URL : '' // https://www.example.com
+	canonical: import.meta.env ? '' : '' // https://www.example.com
 };
