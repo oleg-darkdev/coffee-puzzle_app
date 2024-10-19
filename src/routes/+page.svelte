9be5371b044187ec0f6b04d665b6637f5db66cc1
DarkDev <darkdev_brand@riseup.net>
2024-10-19 08:04:03 +0300

    init basic structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
 
 
-	import { SEO } from '$shared';
-	// import {  } from '$widgets';
-  // import {  } from ''
+	import { SEO, otherBoardgames, navigation, navigation,  } from '$shared';
+	import { Footer, Header, Hero, Cta, Faq, Features, Team, About, PreHeader, Contact, ScreenshotesSlider, AboutAuthor, Steps, BigImgsSlider, OtherBoardgamesSlider,  } from '$widgets';
+  // import {  } from '$entities'
 
   export let data;
-
 
   // console.log(data.sessionid)
 </script>
 
 
 <SEO title="Home"/>
+
+
+
+
+
+
+<Header {navigation}>
+  <PreHeader />
+</Header>
+
+
+<main class="main-wrapper">
+  <div class="section-header23 top-offset">
+    <Hero />
+
+    <!-- <ScreenshotesSlider /> -->
+  </div>
+
+  <Steps />
+
+  <Features />
+
+  <BigImgsSlider />
+
+  <About />
+
+  <Team />
+
+  <AboutAuthor />
+
+  <OtherBoardgamesSlider {otherBoardgames} />
+
+  <Faq />
+
+  <Cta />
+
+  <Contact />
+
+
+</main>
+
+<Footer {otherBoardgames} {navigation}/>
+
 
 
 <style lang="postcss">
